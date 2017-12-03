9fd25a65821af9f1feebb28c4bdd9f1a9f7edefc
alan345 <alan345@gmail.com>
2017-12-03 11:13:58 -0800

    status quote

---
+++ src/app/quote/single/quoteStatus/quoteStatus.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { Quote, StatusQuotes } from '../../quote.model';
+
+@Component({
+  selector: 'app-quote-status',
+  templateUrl: './quoteStatus.component.html',
+  styleUrls: ['./quoteStatus.component.css'],
+})
+export class QuoteStatusComponent {
+  @Input() fetchedQuote: Quote = new Quote();
+  @Input() showSingleSelected: boolean = false;
+  statusQuotes = StatusQuotes;
+  statusQuotesSingleSelected = StatusQuotes;
+
+  constructor() { }
+
+  ngOnChanges() {
+    if(this.showSingleSelected) {
+      this.statusQuotesSingleSelected = [this.statusQuotes.find((statusQuote) => {
+        return statusQuote.indexStatus === this.fetchedQuote.statusQuote;
+      })]
+    }
+  }
+}
