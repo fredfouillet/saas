eadd8b31d9bc55af27b197e65610d90fb3d4a5ea
alan345 <alan345@gmail.com>
2017-12-20 09:44:12 -0800

    ok

--- server/routes/userCalendar.js
+++ server/routes/userCalendar.js
@@ -150,7 +150,7 @@ router.post('/', function(req, res, next) {
 
 // get all forms from database
 router.get('/page/:page', function(req, res, next) {
-  var itemsPerPage = 15
+  var itemsPerPage = 100
   var currentPage = Number(req.params.page)
   var pageNumber = currentPage - 1
   var skip = (itemsPerPage * pageNumber)
